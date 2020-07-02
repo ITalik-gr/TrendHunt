6ae049ce3b79797f6846ac47aae6c2f56a59c7bd
v.ITalik <vitalik.gricenko77@gmail.com>
2020-07-02 15:10:30 +0300

    acordion

--- src/js/script.js
+++ src/js/script.js
@@ -29,21 +29,35 @@
 
 /* jQuery
 ================================================== */
-$(function() {
-  $('.acc__title').click(function(j) {
+// $(function() {
+//   $('.acc__title').click(function(j) {
     
-    var dropDown = $(this).closest('.acc__card').find('.acc__panel');
-    $(this).closest('.acc').find('.acc__panel').not(dropDown).slideUp();
+//     var dropDown = $(this).closest('.acc__card').find('.acc__panel');
+//     $(this).closest('.acc').find('.acc__panel').not(dropDown).slideUp();
     
-    if ($(this).hasClass('active')) {
-      $(this).removeClass('active');
-    } else {
-      $(this).closest('.acc').find('.acc__title.active').removeClass('active');
-      $(this).addClass('active');
-    }
+//     if ($(this).hasClass('active')) {
+//       $(this).removeClass('active');
+//     } else {
+//       $(this).closest('.acc').find('.acc__title.active').removeClass('active');
+//       $(this).addClass('active');
+//     }
     
-    dropDown.stop(false, true).slideToggle();
-    j.preventDefault();
+//     dropDown.stop(false, true).slideToggle();
+//     j.preventDefault();
+//   });
+// });
+
+
+
+
+$(document).ready(function() {
+  $('.accordion__header').click(function() {
+      
+      $(".accordion__body").not($(this).next()).slideUp(400);
+      $(this).next().slideToggle(400);
+      
+      $(".accordion__item").not($(this).closest(".accordion__item")).removeClass("open-accordion");
+      $(this).closest(".accordion__item").toggleClass("open-accordion");
   });
 });
 
