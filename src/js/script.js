ae2076d52f9f11e042525c3934616fbe9ad5ac86
v.ITalik <vitalik.gricenko77@gmail.com>
2020-07-04 20:58:41 +0300

    animate

--- src/js/script.js
+++ src/js/script.js
@@ -1,53 +1,8 @@
 
-// let arrow = document.querySelector('.question-card__img'),
-//     text = document.querySelector('.question-card__text');
 
-// arrow.addEventListener('click', function() {
-//   arrow.classList.toggle('question-card__img-active');
-// });
-// arrow.addEventListener('click', function() {
-//   text.classList.toggle('question-card__text-active');
-// });
-// console.log(text);
-
-// $(document).ready(function() {
-
-//   $('.acordion-item__trigger').click(function() {
-//     $(this).parent('.acordion-item').toggleClass('acordion-item-active');
-//   });
-
-// });
-
-
-
-
-
-
-
-
-
-
-/* jQuery
-================================================== */
-// $(function() {
-//   $('.acc__title').click(function(j) {
-    
-//     var dropDown = $(this).closest('.acc__card').find('.acc__panel');
-//     $(this).closest('.acc').find('.acc__panel').not(dropDown).slideUp();
-    
-//     if ($(this).hasClass('active')) {
-//       $(this).removeClass('active');
-//     } else {
-//       $(this).closest('.acc').find('.acc__title.active').removeClass('active');
-//       $(this).addClass('active');
-//     }
-    
-//     dropDown.stop(false, true).slideToggle();
-//     j.preventDefault();
-//   });
-// });
-
-
+  // $(function(){
+  //   $('.paral').parallax();
+  // });
 
 
 $(document).ready(function() {
@@ -62,11 +17,12 @@ $(document).ready(function() {
 });
 
 
-let bg = document.querySelector('.profit__union-l');
-window.addEventListener('mousemove', function(e) {
-    let x = e.clientX / window.innerWidth;
-    let y = e.clientY / window.innerHeight;  
-    bg.style.transform = 'translate(-' + x * 20 + 'px, -' + y * 20 + 'px)';
-});
+// let bg = document.querySelectorAll('.paral')[1];
+// window.addEventListener('mousemove', function(e) {
+//     let x = e.clientX / window.innerWidth;
+//     let y = e.clientY / window.innerHeight;  
+//     bg.style.transform = 'translate(-' + x * 20 + 'px, -' + y * 20 + 'px)';
+// });
 
 
+// console.log(bg);
