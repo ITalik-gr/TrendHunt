b5f8b960eacc4a0d6c7383462d91cc0632f67b5d
v.ITalik <vitalik.gricenko77@gmail.com>
2020-08-14 11:11:53 +0300

    modal - phpmailer

--- src/mail.php
+++ src/mail.php
@@ -5,7 +5,7 @@ $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
 $name = $_POST['user_name'];
-$surName = $_POST['user_surName'];
+$surName = $_POST['user_surname'];
 $phone = $_POST['user_phone'];
 $email = $_POST['user_email'];
 $link = $_POST['user_link'];
@@ -18,13 +18,13 @@ $promo = $_POST['user_promo'];
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'vitalik.gricenko77@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'https1703'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'test.italik@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'asdfgh890'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('vitalik.gricenko77@gmail.com'); // от кого будет уходить письмо?
-$mail->addAddress('vitkagriceko@gmail.com');     // Кому будет уходить письмо 
+$mail->setFrom('test.italik@gmail.com'); // от кого будет уходить письмо?
+$mail->addAddress('vitkagricenko@gmail.com');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
@@ -34,12 +34,13 @@ $mail->addAddress('vitkagriceko@gmail.com');     // Кому будет уход
 $mail->isHTML(true);                                  // Set email format to HTML
 
 $mail->Subject = 'Заявка с тестового сайта';
-$mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
+$mail->Body    = '' .$name . ' ' . $surName . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email.
+                    '<br> Ссылка на его веб сайт: ' . $link. '<br> Магазин на платформе: ' . $shop . '<br> Описание магазина: ' . $shopDescr . '<br> Промокод пользователя: ' . $promo;
 $mail->AltBody = '';
 
 if(!$mail->send()) {
     return false;
 } else {
-    return true;
+    header('location: index.html');
 }
 ?>
